64cffc60f332557e5f963a48efde6d37c381f3e2
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-15 09:32:32 +0530

    Create not_tb.v

---
+++ DAY 1/logic_gates/NOT_gate/not_tb.v
@@ -0,0 +1,16 @@
+
+module notgatetb;
+ reg a;
+ wire y;
+notgate dut(a,y);
+initial begin
+$monitor("a=%b,y=%b",a,y);
+ a=0;#5;
+ a=1;#5;
+ $display("a=%b,y=%b",a,y);
+ end
+ initial begin
+         $dumpfile("notgate.vcd");
+         $dumpvars;
+ end
+endmodule
