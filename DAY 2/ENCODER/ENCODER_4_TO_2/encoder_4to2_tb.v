cc2c5e8f05b49132c4bc89d1d36dd822a8917639
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-16 19:28:22 +0530

    Update encoder_4to2_tb.v

--- DAY 2/ENCODER/ENCODER_4_TO_2/encoder_4to2_tb.v
+++ DAY 2/ENCODER/ENCODER_4_TO_2/encoder_4to2_tb.v
@@ -10,16 +10,13 @@ module encoder_4_to_2_tb;
  
     initial begin
         $dumpfile("encoder_4_to_2_tb.vcd");
-        $dumpvars(0, encoder_4_to_2_tb);
+        $dumpvars();
   
         in = 4'b0001; #10; 
         in = 4'b0010; #10; 
         in = 4'b0100; #10; 
         in = 4'b1000; #10; 
 
-        in = 4'b0011; #10; 
-        in = 4'b0110; #10;
-        in = 4'b1100; #10;
     
         $display("Simulation complete");
         $finish;
