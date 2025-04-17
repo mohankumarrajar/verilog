24ae13b07ec166b40df4fa7fb684212c37dc9e18
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-17 07:14:51 +0530

    Create odd_check_tb.v

---
+++ DAY 3/Parity_Checker/Odd_Checker/odd_check_tb.v
@@ -0,0 +1,29 @@
+module odd_parity_tb;
+reg a,b,c,p;
+wire o;
+odd_parity h(.a(a),.b(b),.c(c),.p(p),.o(o));
+initial begin
+  $dumpfile("odd_check.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,c=%b,p=%b,o=%b",a,b,c,p,o);
+a=0; b=0; c=0; p=0; 
+#5 a=0; b=0; c=0; p=0; 
+#5 a=0; b=0; c=0; p=1; 
+#5 a=0; b=0; c=1; p=0; 
+#5 a=0; b=0; c=1; p=1; 
+#5 a=0; b=1; c=0; p=0; 
+#5 a=0; b=1; c=0; p=1; 
+#5 a=0; b=1; c=1; p=0; 
+#5 a=0; b=1; c=1; p=1; 
+#5 a=1; b=0; c=0; p=0; 
+#5 a=1; b=0; c=0; p=1; 
+#5 a=1; b=0; c=1; p=0; 
+#5 a=1; b=0; c=1; p=1; 
+#5 a=1; b=1; c=0; p=0; 
+#5 a=1; b=1; c=0; p=1; 
+#5 a=1; b=1; c=1; p=0; 
+#5 a=1; b=1; c=1; p=1; 
+
+
+end
+endmodule
