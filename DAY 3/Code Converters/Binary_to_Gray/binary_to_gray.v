3a89e2c60eb05edaa3773416be48deb9a872e89b
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-18 10:28:41 +0530

    Update binary_to_gray.v

--- DAY 3/Code Converters/Binary_to_Gray/binary_to_gray.v
+++ DAY 3/Code Converters/Binary_to_Gray/binary_to_gray.v
@@ -1,15 +1,6 @@
-module bintogray_tb;
-        parameter n=4;
-        reg[n-1:0]bin;
-        wire [n-1:0]gray;
+module bintogray(
+        input [3:0]bin,
+        output [3:0]gray);
 
-  bintogray bintogray(.bin(bin),.gray(gray));
-  initial begin
-          $dumpfile("bintogray.vcd");
-          $dumpvars();
-          $monitor($time,"bin=%b,gray=%b",bin,gray);
-
-          #5 bin=4'b0010;
-          #5 bin=4'b0100;
-  end
-  endmodule                                                                                                         ~               
+        assign gray = bin^(bin>>1);
+        endmodule                                                                                                       ~               
