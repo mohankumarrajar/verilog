018d47dea7cc543dbe1c16f1dd9b87e4404651e5
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-21 21:04:11 +0530

    Update gray_to_bin_tb.v

--- DAY 3/Code Converters/Gray_to_Binary/gray_to_bin_tb.v
+++ DAY 3/Code Converters/Gray_to_Binary/gray_to_bin_tb.v
@@ -15,17 +15,17 @@ module graytobin_tb;
            #5 gray=4'b0001;
            #5 gray=4'b0011;
            #5 gray=4'b0010;
-           #5 gray=4'b0110;
            #5 gray=4'b0111;
-           #5 gray=4'b0101;
+           #5 gray=4'b0110;
            #5 gray=4'b0100;
-           #5 gray=4'b1100;
-           #5 gray=4'b1101;
+           #5 gray=4'b0101;
            #5 gray=4'b1111;
            #5 gray=4'b1110;
-           #5 gray=4'b1010;
-           #5 gray=4'b1011;
-           #5 gray=4'b1001;
+           #5 gray=4'b1100;
+           #5 gray=4'b1101;
            #5 gray=4'b1000;
+           #5 gray=4'b1001;
+           #5 gray=4'b1011;
+           #5 gray=4'b1010;
    end
    endmodule
