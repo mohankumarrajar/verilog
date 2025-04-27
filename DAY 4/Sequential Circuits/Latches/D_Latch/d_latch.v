88a826ddccb7e9ecf806a2df2312f01eea042635
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-27 19:49:39 +0530

    Update d_latch.v

--- DAY 4/Sequential Circuits/Latches/D_Latch/d_latch.v
+++ DAY 4/Sequential Circuits/Latches/D_Latch/d_latch.v
@@ -1,1 +1,12 @@
+module d_latch (
+    input d,
+    output reg q,
+    output q_bar );
 
+      assign q_bar = d;
+      always @(*) begin
+        if (d)
+            q <= d;
+     end
+
+endmodule
