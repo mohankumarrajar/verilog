2cfd5779461af1e954d5afa3ba07ef84456710c1
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-27 19:50:40 +0530

    Create d_latch_tb.v

---
+++ DAY 4/Sequential Circuits/Latches/D_Latch/d_latch_tb.v
@@ -0,0 +1,21 @@
+module d_latch_tb;
+    reg d;
+    wire q;
+    wire q_bar;
+
+    d_latch uut (.d(d),.q(q),.q_bar(q_bar)
+    );
+
+        initial begin
+        d = 0;
+
+           $dumpfile("d_latch_tb.vcd");
+        $dumpvars();
+
+        $monitor($time,"d=%b,,q=%b,q_bar=%b",d,q,q_bar);
+         #10    d = 1;
+
+       #10      d = 0;
+
+         end
+endmodule
