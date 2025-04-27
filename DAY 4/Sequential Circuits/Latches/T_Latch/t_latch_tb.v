5c90a5ab152a9aad1ad45fc81e272773b3d9b422
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-27 20:22:11 +0530

    Create t_latch_tb.v

---
+++ DAY 4/Sequential Circuits/Latches/T_Latch/t_latch_tb.v
@@ -0,0 +1,14 @@
+module tb;
+reg t;
+wire q,qb;
+t uut(.t(t),.q(q),.qb(qb));
+initial begin
+$dumpfile("t.vcd");
+$dumpvars;
+$monitor ($time,"t=%b,q=%b,qb=%b",t,q,qb);
+t=0;
+#5 t=0;
+#5 t=1;
+  #5 t=0;
+end
+endmodule
