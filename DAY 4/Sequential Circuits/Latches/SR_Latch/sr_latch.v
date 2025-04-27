46db2a3a5165047fb015dbdfdaf83417abb73c58
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-27 19:27:35 +0530

    Update sr_latch.v

--- DAY 4/Sequential Circuits/Latches/SR_Latch/sr_latch.v
+++ DAY 4/Sequential Circuits/Latches/SR_Latch/sr_latch.v
@@ -1,1 +1,19 @@
+module sr_latch (
+    input S,
+    input R,
+    output reg Q,
+    output Q_bar
+);
 
+   assign Q_bar=Q;
+
+    always @(S or R) begin
+        case ({S, R})
+            2'b00: Q = Q;
+            2'b01: Q = 1'b0;
+            2'b10: Q = 1'b1;
+            2'b11: Q = 1'bx;
+        endcase
+    end
+
+endmodule
