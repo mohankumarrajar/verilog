dcce6d110c0807de435c607d1448cc97aedcbb37
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-30 22:28:04 +0530

    Update d_ff_tb.v

--- DAY 5/FILP_FLOP/D_FF/d_ff_tb.v
+++ DAY 5/FILP_FLOP/D_FF/d_ff_tb.v
@@ -1,1 +1,21 @@
-
+module dff_tb;
+reg d,clk,rst;
+wire q,qb;
+  dff uut(d,clk,rst,q,qb);
+initial begin
+  $dumpfile ("dff.vcd");
+$dumpvars;
+end
+initial begin
+clk=1;
+forever #5 clk=~clk;
+end
+initial begin
+#10 d=0; rst=1; rst=0; #15;
+  $display($time,"d=%b,clk=%b,rst=%b,q=%b,qb=%b",d,clk,rst,q,qb); #5
+#15 d=1; #18;
+  $display($time,"d=%b,clk=%b,rst=%b,q=%b,qb=%b",d,clk,rst,q,qb); #10
+#80;
+$finish;
+end
+endmodule
