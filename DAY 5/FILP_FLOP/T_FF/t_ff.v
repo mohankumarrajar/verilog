9726843c53a84ee6182a93d5c521d6eb56c99ea1
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-30 22:17:31 +0530

    Update t_ff.v

--- DAY 5/FILP_FLOP/T_FF/t_ff.v
+++ DAY 5/FILP_FLOP/T_FF/t_ff.v
@@ -1,1 +1,14 @@
-
+module tff(
+  input t,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+  if (rst) begin
+q<=1;
+end
+else
+case({t})
+1'b0 : q<=1'b0; 
+1'b1 : q<=1'b1; 
+endcase
+end
+assign qb=~q;
+endmodule
