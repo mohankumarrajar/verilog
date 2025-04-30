6a4a06239452810e92eeafdeebe6fe3c5ce2825f
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-04-30 21:54:48 +0530

    Update sr_ff.v

--- DAY 5/FILP_FLOP/SR_FF/sr_ff.v
+++ DAY 5/FILP_FLOP/SR_FF/sr_ff.v
@@ -1,1 +1,15 @@
-
+module srff(input s,r,clk,rst,output reg q,output qb);
+always @ (posedge clk) begin
+        if (rst) begin
+q<=1;
+end
+else
+casex ({s,r})
+2'b00 :q<=q; 
+2'b01 :q<=1'b0; 
+2'b10 :q<=1'b1; 
+2'b11 :q<=1'bx; 
+endcase
+end
+assign qb=~q;
+endmodule
