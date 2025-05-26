0f4950bb6a06955921c4116a9591fc41231408e1
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-26 12:31:23 +0530

    Create seven_tb.v

---
+++ DAY 6/7_SEGMENT/seven_tb.v
@@ -0,0 +1,19 @@
+module alu_tb;
+reg [3:0]bcd;
+wire[6:0]led;
+integer i;
+seven ut(
+        .bcd(bcd),.led(led));
+
+initial begin
+        $dumpfile("a.vcd");
+        $dumpvars();
+
+        $monitor($time,"bcd=%b,led=%b",bcd,led);
+
+        for (i=0;i<15;i=i+1) begin
+                bcd=i;
+                #10;
+        end
+end
+endmodule
