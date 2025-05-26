a79faf24027992f72e1a64ed520a826edcd3d0c6
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-26 17:46:41 +0530

    Update piso.v

--- DAY 7/Registers/PISO/piso.v
+++ DAY 7/Registers/PISO/piso.v
@@ -1,1 +1,19 @@
+module piso(
+        input [3:0]pi,input clk,clr,load,
+        output reg so);
 
+reg [3:0]store;
+
+always@(posedge clk)begin
+        if(clr==1)begin
+                store <=4'b0000;
+        end
+        else if (load!=1)begin
+store <= pi;
+end
+else begin
+so<= store[0];
+store = store >>1;
+end
+end
+endmodule
