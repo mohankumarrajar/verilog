04eb5bdea74d5134c09002f722169898081e42e7
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-26 08:15:45 +0530

    Update jk_ff.v

--- DAY 5/FILP_FLOP/JK_FF/jk_ff.v
+++ DAY 5/FILP_FLOP/JK_FF/jk_ff.v
@@ -1,25 +1,15 @@
-module jkfftb;
-reg j,k,clk,rs;
-wire q,qb;
-jkff uut(j,k,clk,rs,q,qb);
-initial begin
-$dumpfile("jk.vcd");
-$dumpvars;
+module jkff(input j,k,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+if (rs) begin
+q<=1;
 end
-initial begin
-clk=1;
-forever #5 clk=~clk;
+else
+case ({j,k})
+2'b00 : q<=q; //nochange
+2'b01 : q<=1'b0; // reset
+2'b10 : q<=1'b1; //set
+2'b11 : q<=1'bx; //toggle
+endcase
 end
-initial begin
-#10 j=0; k=0; #15;
-$display($time,"j=%b,k=%b,clk=%b,q=%b,qb=%b",j,k,clk,q,qb);
-#15 j=0; k=1;  #30;
-$display($time,"j=%b,k=%b,clk=%b,q=%b,qb=%b",j,k,clk,q,qb);
-#20 j=1; k=0;  #45;
-$display($time,"j=%b,k=%b,clk=%b,q=%b,qb=%b",j,k,clk,q,qb);
-#25 j=1; k=1;  #50;
-$display($time,"j=%b,k=%b,clk=%b,q=%b,qb=%b",j,k,clk,q,qb);
-#95;
-$finish;
-end
+assign qb=~q;
 endmodule
