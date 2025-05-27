7f00b4950ba240741960039d3177802a0abeffb7
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-27 10:21:03 +0530

    Update ripple_adder_tb.v

--- DAY 8/RIPPLE ADDER/ripple_adder_tb.v
+++ DAY 8/RIPPLE ADDER/ripple_adder_tb.v
@@ -1,1 +1,31 @@
+module ra_tb;
 
+    reg [3:0] a, b;
+    reg cin;
+    wire [3:0] sum;
+    wire cout;
+
+
+    ra uut (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .sum(sum),
+        .cout(cout)
+    );
+
+    initial begin
+
+        $monitor("a=%b,b=%b,cin=%b,sum=%b,cout=%b", $time, a, b, cin, sum, cout);
+
+
+        a = 4'b0000; b = 4'b0000; cin = 0; #10;
+        a = 4'b0011; b = 4'b0101; cin = 0; #10;
+        a = 4'b1111; b = 4'b0001; cin = 0; #10;
+        a = 4'b1010; b = 4'b0101; cin = 1; #10;
+        a = 4'b1111; b = 4'b1111; cin = 1; #10;
+
+        $finish;
+    end
+
+endmodule
