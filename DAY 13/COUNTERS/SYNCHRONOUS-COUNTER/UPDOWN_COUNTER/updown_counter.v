e4a413064cc4502186135c357d3b37ab18a27f15
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 13:58:18 +0530

    Update updown_counter.v

--- DAY 13/COUNTERS/SYNCHRONOUS-COUNTER/UPDOWN_COUNTER/updown_counter.v
+++ DAY 13/COUNTERS/SYNCHRONOUS-COUNTER/UPDOWN_COUNTER/updown_counter.v
@@ -1,1 +1,14 @@
+module updo(
+        input clk,rst,up,
+        output reg[3:0]count);
 
+  always@(posedge clk)begin
+          if(rst==0)begin
+                  count<=4'b0;
+  end
+  else begin
+          if(up) count<=count+1'b1;
+          else count<=count-1'b1;
+  end
+end
+endmodule
