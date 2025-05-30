26cbd62aac46dec8da73eb0240a6755730511515
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 09:26:36 +0530

    Update 11011.v

--- DAY 11/FSM/MOORE NON OVERLAPPING/11011/11011.v
+++ DAY 11/FSM/MOORE NON OVERLAPPING/11011/11011.v
@@ -1,1 +1,34 @@
+module moore_11011(
+        input in,clk,rst,
+        output out);
 
+parameter a = 4'd1;
+parameter b = 4'd2;
+parameter c = 4'd3;
+parameter d = 4'd4;
+parameter e = 4'd5;
+parameter f = 4'd6;
+
+  reg[5:0] state,next_state;
+
+always@(posedge clk or negedge rst)begin
+
+        if (!rst)begin
+                state<=a;
+        end
+        else
+                state<=next_state;
+end
+always @ (*)begin
+case (state)
+a : next_state = in ? b : a;
+b : next_state = in ? c : a;
+c : next_state = in ? c : d;
+d : next_state = in ? e : a;
+e : next_state = in ? f : a;
+f : next_state = in ? b : a;
+default : next_state = a;
+endcase
+end
+assign out = (state==f) && (in==1);
+endmodule
