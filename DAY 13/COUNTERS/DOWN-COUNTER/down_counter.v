f6c444e87305c0722f50bcf777c83e4ae5bee05d
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 13:40:46 +0530

    Update down_counter.v

--- DAY 13/COUNTERS/DOWN-COUNTER/down_counter.v
+++ DAY 13/COUNTERS/DOWN-COUNTER/down_counter.v
@@ -1,1 +1,11 @@
+module down(
+        input clk,rst,
+        output reg [3:0]count);
+  always@(posedge clk) begin
 
+          if(rst==0)
+                  count<=3'b111;
+          else
+                  count<=count+3'b001;
+  end
+  endmodule
