e6903097aa6989343b80973f359081aa035971fe
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 08:33:34 +0530

    Create 11011_tb.v

---
+++ DAY 10/FSM/MEALAY NON OVERLAPPING/11011/11011_tb.v
@@ -0,0 +1,43 @@
+module mealay_11011_tb;
+reg in,clk,rst;
+wire out;
+mealay_11011 uut(in,clk,rst,out);
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+  $dumpfile("11011.vcd");
+$dumpvars;
+$monitor($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+
+in=0;
+#5 rst =0; #10 rst=1;
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+
+ 
+
+#15;
+$finish;
+end
+endmodule
