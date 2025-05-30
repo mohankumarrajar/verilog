ef87641e59420ac6d982390915c1b054947d026e
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 13:41:18 +0530

    Update down_counter_tb.v

--- DAY 13/COUNTERS/DOWN-COUNTER/down_counter_tb.v
+++ DAY 13/COUNTERS/DOWN-COUNTER/down_counter_tb.v
@@ -1,1 +1,21 @@
+module down_tb;
+reg clk,rst;
+wire [3:0]count;
 
+down uut (.clk(clk),.rst(rst),.count(count));
+
+initial begin
+        $dumpfile("gaot.vcd");
+        $dumpvars();
+        $monitor($time,"clk=%b,rst=%b,count=%b",clk,rst,count);
+
+         clk=0;
+         #5 rst=0;
+         #5 rst=1;
+
+         #45;
+         $finish;
+ end
+         always #5 clk=~clk;
+
+ endmodule
