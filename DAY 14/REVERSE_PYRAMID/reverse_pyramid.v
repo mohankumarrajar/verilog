3cce2c7397861b80b52f09bd840bd606041a0a4b
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-05-30 10:03:34 +0530

    Create reverse_pyramid.v

---
+++ DAY 14/REVERSE_PYRAMID/reverse_pyramid.v
@@ -0,0 +1,14 @@
+module reverse_pyramid;
+integer i,j,space,rows;
+
+initial begin
+        rows=5;
+        for(i=0;i<rows;i=i+1)begin
+                for(space=0;space<i;space=space+1)
+                        $write("");
+                for(j=0;j<(2*(rows-i)-1);j=j+1)
+                        $write("*");
+                $display("");
+        end
+end
+endmodule
